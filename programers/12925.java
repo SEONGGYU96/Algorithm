74926373a5560e63b5c3e77b24a28c4882755aff
김성규 <link5658@gmail.com>
2020-07-24 01:20:03 +0900

    2020/07/24 문자열을 정수로 바꾸기

---
+++ programers/12925.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        boolean isPositive;
+        
+        if (s.charAt(0) == '-') {
+            answer = Integer.parseInt(s.substring(1)) * -1;
+        } else if (s.charAt(0) == '+') {
+            answer = Integer.parseInt(s.substring(1));
+        } else {
+            answer = Integer.parseInt(s);
+        }
+        
+        return answer;
+    }
+}
