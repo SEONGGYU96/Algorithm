ba8b0b70963945dd2bac6aa8544d8cb69f456966
김성규 <link5658@gmail.com>
2020-09-27 03:16:33 +0900

    2020/09/27 이친수

---
+++ BaekJoon/2193.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[N + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        System.out.println(solution(dp, N));
+    }
+
+    private static long solution(long[] dp, int N) {
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[N];
+    }
+}
