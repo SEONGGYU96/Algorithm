320e6e89a9f6fd44a6bb90713c92a7d7caaa91e2
KIM SEONGGYU <link5658@gmail.com>
2021-02-06 11:13:32 +0900

    2021/02/06 무한 수열

--- BaekJoon/1351.java
+++ BaekJoon/1351.java
@@ -1,4 +1,34 @@
-package com.gyugyu;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int P;
+    private static int Q;
+    private static HashMap<Long, Long> A;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long N = Long.parseLong(st.nextToken());
+        P = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+
+        A = new HashMap<>();
+        A.put(0L, 1L);
+
+        System.out.println(getValueFromA(N));
+    }
+
+    private static long getValueFromA(long index) {
+        if (!A.containsKey(index)) {
+            A.put(index, getValueFromA(index / (long) P) + getValueFromA(index / (long) Q));
+        }
+        return A.get(index);
+    }
+}
+ggpackage com.gyugyu;
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -38,3 +68,4 @@ public class Main {
         }
     }
 }
+
