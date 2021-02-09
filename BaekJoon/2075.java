56340040119f53678088e713191d8da62c65df10
KIM SEONGGYU <link5658@gmail.com>
2021-02-09 11:08:18 +0900

    2021/02/09 N번째 큰 수

---
+++ BaekJoon/2075.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                queue.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer = queue.poll();
+        }
+        System.out.println(answer);
+    }
+}
+
