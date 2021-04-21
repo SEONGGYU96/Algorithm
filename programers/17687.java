900334a50e650cfe0b14469b872f9e361fd18c45
KIM SEONGGYU <link5658@gmail.com>
2021-04-21 14:02:40 +0900

    2021/04/21 n진수 게임

--- programers/17687.java
+++ programers/17687.java
@@ -1,44 +1,21 @@
-import java.util.*;
-
 class Solution {
-
-    String[] dp;
-
     public String solution(int n, int t, int m, int p) {
-        String answer = "";
-        dp = new String[p + m * (t - 1)];
+        StringBuilder answer = new StringBuilder();
 
-        String temp = "";
-        for (int i = 0; temp.length() < p + m * (t - 1); i++) {
-            temp += convert(n, i);
-        }
-        System.out.println(temp);
+        int startNumber = 0;
 
-        for (int i = p, count = 0; count < t; i += m, count++) {
-            answer += temp.charAt(i - 1);
-        }
-
-        return answer;
-    }
-
-    private String convert(int base, int n) {
-        if (n == 0) {
-            return "0";
-        }
-        Stack<Integer> stack = new Stack<>();
-        while (n > 0) {
-            stack.push(n % base);
-            n /= base;
-        }
         StringBuilder builder = new StringBuilder();
-        while (!stack.isEmpty()) {
-            int current = stack.pop();
-            if (current >= 10) {
-                builder.append((char) (65 + current - 10));
-            } else {
-                builder.append(current);
-            }
+
+        while (builder.length() < m * t) {
+            builder.append(Integer.toString(startNumber++, n));
         }
-        return builder.toString();
+
+        String temp = builder.toString();
+
+        for (int i = 0; i < t; i++) {
+            answer.append(temp.charAt(i * m + p - 1));
+        }
+
+        return answer.toString().toUpperCase();
     }
 }
