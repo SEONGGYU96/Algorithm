ec682479d45f76932a07d04786da3eacc42165e3
KIM SEONGGYU <link5658@gmail.com>
2021-04-26 09:57:47 +0900

    2021/04/26 되돌리기

--- BaekJoon/1360.java
+++ BaekJoon/1360.java
@@ -1,10 +1,13 @@
+import java.io.*;
+import java.util.*;
+
 class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        ArrayList<String> history = new ArrayList<>();
+        ArrayList<History> histories = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             String command = st.nextToken();
@@ -12,20 +15,36 @@ class Main {
             int at = Integer.parseInt(st.nextToken());
 
             if (command.equals("type")) {
-                if (history.isEmpty()) {
-                    history.add(arg);
+                if (histories.isEmpty()) {
+                    histories.add(new History(at, arg));
                 } else {
-                    history.add(history.get(history.size() - 1) + arg);
+                    histories.add(new History(at, histories.get(histories.size() - 1).value + arg));
                 }
             } else {
                 int backTime = Integer.parseInt(arg);
-                history.add(history.get(Math.max(0, at - backTime - 1)));
-                
-                for (int j = history.size() - 1; j >= 0; j--) {
-                }
+                histories.add(new History(at, getHistory(histories, at - backTime)));
             }
-
         }
-        System.out.println(history.get(history.size() - 1));
+        System.out.println(histories.get(histories.size() - 1).value);
+    }
+
+    private static String getHistory(ArrayList<History> histories, int targetTime) {
+        for (int i = histories.size() - 1; i >= 0; i--) {
+            if (histories.get(i).time < targetTime) {
+                return histories.get(i).value;
+            }
+        }
+        return "";
+    }
+
+    private static class History {
+        private final int time;
+        private final String value;
+
+        public History(int time, String value) {
+            this.time = time;
+            this.value = value;
+        }
     }
 }
+
